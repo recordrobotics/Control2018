7ac7f55681b97b398739af5c8c2cb29ad1373d91
fingoldin <vassilioskaxiras@gmail.com>
2018-02-10 16:22:47 +0200

    initial commit

---
+++ src/org/usfirst/frc/team6731/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team6731.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
+	public static int motor1Port = 1;
+	public static int motor2Port = 2;
+	public static int motor3Port = 3;
+	public static int motor4Port = 4;
+
+	public static int joystickPort = 5;
+	
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
+}
